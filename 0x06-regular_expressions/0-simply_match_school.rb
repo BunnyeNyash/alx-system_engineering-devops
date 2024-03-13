89b0fcee81b9716a92d5c8eef3f6a5ee5a24f2ab
BunnyeNyash <otachibosibori@gmail.com>
2024-03-13 12:37:48 -0700

    A Ruby script that uses a regular expression that must match School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# A Ruby script employing a regular expression that must match the term 'School'
+# A Ruby script employing a regular expression that must match School
 puts ARGV[0].scan(/School/).join
