723abb3a403415929fbb4c75ac92dda3eed703dc
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-06 15:32:44 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,50 +1,17 @@
 # 2-puppet_custom_http_response_header.pp
 # Configures a new Ubuntu machine to add a custom Nginx response header X-Served-By with the server hostname
 
-# Ensure Nginx package is installed
-package { 'nginx':
-  ensure => installed,
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-# Get the hostname
-$hostname = $::hostname
-
-# Configure Nginx default site to include custom header
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Template for Nginx configuration
-file { '/etc/puppetlabs/code/environments/production/templates/nginx/default.erb':
-  ensure  => file,
-  content => @("END")
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name _;
-    add_header X-Served-By <%= @hostname %>;
-    root /var/www/html;
-    index index.html;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-END
-}
-
-# Create a basic index.html
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  require    => Package['nginx'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
